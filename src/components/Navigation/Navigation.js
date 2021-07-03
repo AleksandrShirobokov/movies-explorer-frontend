0580b8040d62267c234a19ea126ef53f96230198
Aleksandr Shirobokov <scenarist95@yandex.ru>
2021-07-04 02:12:06 +1100

    Front third rewie

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,8 +2,15 @@ import React from 'react';
 import { Link, Route, Switch } from 'react-router-dom';
 import './Navigation.css';
 import logo from '../../images/profile-logo.svg'
+import account__icon from '../../images/profile-logo.svg'
 
-export default function Navigation(props) {
+export default function Navigation({ loggedIn }) {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  function toggleBurgerMenu() {
+    setIsMenuOpen(!isMenuOpen);
+  }
+
   return (
     <div className='navigation__container'>
       <Switch>
@@ -23,6 +30,21 @@ export default function Navigation(props) {
             <Link to="/profile" className="navigation__link navigation__link_type_profile">Аккаунт</Link>
             <img className="navigation__link_logo" src={logo} alt='logo' />
           </div>
+          <button className={`navigation__burger-button ${loggedIn ? '' : 'no-display'}`} onClick={ toggleBurgerMenu } />
+      <div className={`navigation__burger-menu ${isMenuOpen ? 'visible' : ''}`}>
+        <button className='navigation__burger-button navigation__burger-button_type_close' onClick={ toggleBurgerMenu } />
+        <div className={`navigation__burger-menu-container ${isMenuOpen ? 'visible' : ''}`}>
+          <div className='navigation__burger-links-block'>
+            <Link to='/' className='navigation__link navigation__burger-link' onClick={ toggleBurgerMenu }>Главная</Link>
+            <Link to='/movies' className='navigation__link navigation__burger-link' onClick={ toggleBurgerMenu }>Фильмы</Link>
+            <Link to='/saved-movies' className='navigation__link navigation__burger-link' onClick={ toggleBurgerMenu }>Сохранённые фильмы</Link>
+            <Link to='/profile' className='navigation__link navigation__burger-link navigation__burger-link_type_account' onClick={ toggleBurgerMenu }>
+              Аккаунт
+              <img alt='Редактировать аккаунт' className='navigation__account-icon' src={account__icon} />
+            </Link>
+          </div>
+        </div>
+      </div>
         </Route>
 
         <Route exact path="/movies">
@@ -34,6 +56,21 @@ export default function Navigation(props) {
             <Link to="/profile" className="navigation__link navigation__link_type_profile">Аккаунт</Link>
             <img className="navigation__link_logo" src={logo} alt='logo' />
           </div>
+          <button className={`navigation__burger-button ${loggedIn ? '' : 'no-display'}`} onClick={ toggleBurgerMenu } />
+      <div className={`navigation__burger-menu ${isMenuOpen ? 'visible' : ''}`}>
+        <button className='navigation__burger-button navigation__burger-button_type_close' onClick={ toggleBurgerMenu } />
+        <div className={`navigation__burger-menu-container ${isMenuOpen ? 'visible' : ''}`}>
+          <div className='navigation__burger-links-block'>
+            <Link to='/' className='navigation__link navigation__burger-link' onClick={ toggleBurgerMenu }>Главная</Link>
+            <Link to='/movies' className='navigation__link navigation__burger-link' onClick={ toggleBurgerMenu }>Фильмы</Link>
+            <Link to='/saved-movies' className='navigation__link navigation__burger-link' onClick={ toggleBurgerMenu }>Сохранённые фильмы</Link>
+            <Link to='/profile' className='navigation__link navigation__burger-link navigation__burger-link_type_account' onClick={ toggleBurgerMenu }>
+              Аккаунт
+              <img alt='Редактировать аккаунт' className='navigation__account-icon' src={account__icon} />
+            </Link>
+          </div>
+        </div>
+      </div>
         </Route>
 
         <Route exact path="/saved-movies">
@@ -45,6 +82,21 @@ export default function Navigation(props) {
             <Link to="/profile" className="navigation__link navigation__link_type_profile">Аккаунт</Link>
             <img className="navigation__link_logo" src={logo} alt='logo' />
           </div>
+          <button className={`navigation__burger-button ${loggedIn ? '' : 'no-display'}`} onClick={ toggleBurgerMenu } />
+      <div className={`navigation__burger-menu ${isMenuOpen ? 'visible' : ''}`}>
+        <button className='navigation__burger-button navigation__burger-button_type_close' onClick={ toggleBurgerMenu } />
+        <div className={`navigation__burger-menu-container ${isMenuOpen ? 'visible' : ''}`}>
+          <div className='navigation__burger-links-block'>
+            <Link to='/' className='navigation__link navigation__burger-link' onClick={ toggleBurgerMenu }>Главная</Link>
+            <Link to='/movies' className='navigation__link navigation__burger-link' onClick={ toggleBurgerMenu }>Фильмы</Link>
+            <Link to='/saved-movies' className='navigation__link navigation__burger-link' onClick={ toggleBurgerMenu }>Сохранённые фильмы</Link>
+            <Link to='/profile' className='navigation__link navigation__burger-link navigation__burger-link_type_account' onClick={ toggleBurgerMenu }>
+              Аккаунт
+              <img alt='Редактировать аккаунт' className='navigation__account-icon' src={account__icon} />
+            </Link>
+          </div>
+        </div>
+      </div>
         </Route>
 
       </Switch>
